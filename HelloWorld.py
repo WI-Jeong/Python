048055a4307eb3554d2fb6aaaffaa40944a77596
WI-Jeong <cpdls23@naver.com>
2023-08-28 20:56:04 +0900

    Python str

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,75 @@
-print("Hello World")
+#print("Hello World")
+#print('Hello World')
+#print("""Hello World""")
+#print('''Hello World''')
+
+#a="Python's favorite food is perl"
+#b='Python"s favorite food is perl'
+#c='Python\'s favorite food is perl'
+#d="\"Python is very easy.\" he says."
+
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+
+#print("Life is too short\nYou need python.")
+#print("""Life is too short
+#You need python.""")
+#print('''Life is too short
+#You need python.''')
+
+#print("Life is too short\tYou need python.")
+#print("Life is too short\\You need python.")
+#print("Life is too short You need \'python\'.")
+#print("Life is too short You need \"python\".")
+
+a="Apple"
+b="is delicious."
+
+
+#print(a+b)
+#print(a*3)
+
+#a="Hello Python World!"
+#print(a[0])
+#print(a[8])
+#print(a[-5])
+
+#print(5>10)
+#print(True)
+#print(not True)
+#print(not(5>10))
+
+#a="Life is too short, You need python."
+#print(a[0:4])
+
+#print(a[:5])
+
+#print(a[15:])
+
+#print(a[::2])
+
+#print(a[::-3])
+
+#number=10
+#day="three"
+
+#a="I ate %d apples.So I was sick for %s days" %(number, day)
+#b="I ate %s apples.So I was sick for %s days" %(1.2, day)
+#print(a)
+#print(b)
+
+
+#a="adsf adfasdfe aefaefawf {} adfadfad".format("안녕")
+#print(a)
+
+
+#a="%0.4f" % 3.421334234
+#b="%10.4f"% 3.421334234
+
+#print(a)
+#print(b)
+
+
+
